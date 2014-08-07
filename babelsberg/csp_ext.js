3222ad9de3157e9d07252756b6aaf27cb7911b8c
Stefan Lehmann <stefan.lehmann@student.hpi.uni-potsdam.de>
2014-08-07 12:57:43 +0200

    add string and number-specific support

--- babelsberg/csp_ext.js
+++ babelsberg/csp_ext.js
@@ -1,7 +1,7 @@
 module('users.timfelgentreff.babelsberg.csp_ext').requires('users.timfelgentreff.csp.csp').toRun(function() {
 	
 	JSLoader.loadJs(module('users.timfelgentreff.csp.underscore-min').uri());
-
+	
 	Object.subclass("csp.Solver", {
 	    isConstraintObject: function() {
 	        return true;
@@ -10,7 +10,7 @@ module('users.timfelgentreff.babelsberg.csp_ext').requires('users.timfelgentreff
 		initialize: function() {
 			this.p = new _csp.DiscreteProblem();
 		},
-		newVariable: function(obj, varname, domain) {
+		newDomain: function(obj, varname, domain) {
 			if(domain.length === 0) {
 				throw "Empty domain not allowed";
 			}
@@ -19,7 +19,6 @@ module('users.timfelgentreff.babelsberg.csp_ext').requires('users.timfelgentreff
 			Constraint.current = { solver: this };
 			
 			this._current = {
-				obj: obj,
 				varname: varname,
 				domain: domain
 			};
@@ -32,14 +31,39 @@ module('users.timfelgentreff.babelsberg.csp_ext').requires('users.timfelgentreff
 			
             delete this._current;
 		},
-		constraintVariableFor: function(value, ivarname) {
+		constraintVariableFor: function(value, ivarname, cobj) {
+			if (ivarname === csp.Solver.DomainMethod) {
+				if (!cobj.parentConstrainedVariable) {
+					debugger
+					throw "can only assign a domain to a property!"					
+				} else {
+					// we actually want to constraint the parent of this property
+					theParent  = cobj.obj;
+					this.__takeNext__ = true;
+					cParent = this.constraintVariableFor(cobj.obj, cobj.parentConstrainedVariable.ivarname, cobj.parentConstrainedVariable);
+					cobj.parentConstrainedVariable.externalVariables(this, cParent);
+					// XXX HACK?
+					delete cParent.__cvar__;
+					return cParent;
+				}
+			}
+			
+			if (this.__takeNext__) {
+				delete this.__takeNext__;
+				return new csp.Variable(
+					this,
+					ivarname,
+					value,
+					[value]
+				);
+			}
+			
 			if(!this._current) {
 				return null;
 			}
 
 			var vari = new csp.Variable(
 				this,
-				this._current.obj,
 				this._current.varname,
 				value,
 				this._current.domain
@@ -48,16 +72,24 @@ module('users.timfelgentreff.babelsberg.csp_ext').requires('users.timfelgentreff
 	    },
 	    weight: 1000,
 	    always: function (opts, func) {
-	    	var constraint  = this.p.addConstraint([], func);
-	    	var satisfiable = this.p.getSolution({});
-	    	if(!satisfiable) {
-	    		this.p.removeConstraint(constraint);
-	    		throw "constraint cannot be satisfied";
+	    	func.varMapping = opts.ctx;
+	    	func.allowUnsolvableOperations = true;
+	    	var cobj = new Constraint(func, this);
+	    	if(!this.__domainDefinition__) {
+		    	var constraint  = this.p.addConstraint([], func);
+		    	var satisfiable = this.p.getSolution({});
+		    	if(!satisfiable) {
+		    		this.p.removeConstraint(constraint);
+		    		throw "constraint cannot be satisfied";
+		    	}
+	    	} else {
+	    		delete this.__domainDefinition__;
 	    	}
 	    }
 	});
 	Object.extend(csp.Solver, {
 		weight: 1000,
+		DomainMethod: "is",
 		uniqueName: 42,
 		getUniqueName: function() {
 			return 'var' + csp.Solver.uniqueName++;
@@ -69,16 +101,23 @@ module('users.timfelgentreff.babelsberg.csp_ext').requires('users.timfelgentreff
 	        return true;
 	    },
 
-		initialize: function(solver, obj, varname, value, domain) {
+		initialize: function(solver, varname, value, domain) {
 			this.solver = solver;
-			this.obj = obj;
 			this.varname = varname;
+			this.setDomain(domain, value);
+		},
+		
+		setDomain: function(domain, currentValue) {
+			if (this.domain) {
+				if(!this.cspname) throw "inconsistent csp.Variable";
+				currentValue = currentValue || this.value();
+				this.solver.p.removeVariable(this.cspname, this.domain);
+			}
 			this.domain = domain;
 			this.cspname = csp.Solver.getUniqueName();
-			
 			this.cspvariable = this.solver.p.addVariable(this.cspname, domain);
 			
-			var valueToAssign = this.domain.indexOf(value) > -1 ? value : this.domain[0];
+			var valueToAssign = this.domain.indexOf(currentValue) > -1 ? currentValue : this.domain[0];
 			this.solver.p.solver.assignments[this.cspname] = valueToAssign;
 		},
 		
@@ -112,7 +151,21 @@ module('users.timfelgentreff.babelsberg.csp_ext').requires('users.timfelgentreff
 	    setReadonly: function(bool) { /* ignored */ },
 	    isReadonly: function() {
 	        return false;
+	    },
+	    
+	    cnIn: function(domain) {
+	    	this.setDomain(domain);
+	    	this.solver.__domainDefinition__ = true;
+	    	return true;
 	    }
+	});
+	
+
+	Number.prototype.__defineGetter__(csp.Solver.DomainMethod, function () {
+		return this;
+	});
+	String.prototype.__defineGetter__(csp.Solver.DomainMethod, function () {
+		return this;
 	});
 
 }); // end of module
