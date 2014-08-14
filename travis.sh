fef6cf50234aaba49cdc2da4f32870e3dd75d2d7
Stefan Lehmann <stefan.lehmann@student.hpi.uni-potsdam.de>
2014-08-14 11:57:19 +0200

    scanning babelsberg subdirectory for docs

--- travis.sh
+++ travis.sh
@@ -5,13 +5,12 @@ if [ "$TYPE" == "Lively" ]; then
     ln -s $PWD LivelyKernel/users/timfelgentreff
 	
     npm install jsdoc@"<=3.3.0"
-	./node_modules/.bin/jsdoc babelsberg/constraintinterpreter.js -d docs
+	./node_modules/.bin/jsdoc babelsberg -r -d docs
 	cd docs
 	curl -T Babelsberg.html http://www.lively-kernel.org/babelsberg/docs/
 	for f in *.html
 	do
 	  echo $f
-	  curl -T $f http://www.lively-kernel.org/babelsberg/docs/
 	done
 	cd ..
     
