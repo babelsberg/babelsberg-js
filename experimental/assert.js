d8d69201becad4dcb44e8debef793f33efe522df
Stefan Lehmann <stefan.lehmann@student.hpi.uni-potsdam.de>
2014-09-06 10:33:35 +0200

    refactoring to abstract ReactiveSolver

--- experimental/assert.js
+++ experimental/assert.js
@@ -3,78 +3,24 @@ module('users.timfelgentreff.experimental.assert').requires('users.timfelgentref
 	JSLoader.loadJs(module('users.timfelgentreff.csp.underscore-min').uri());
 
 	/***************************************************************
-	 * Continuous asserts
+	 * Abstract Reactive Solver
 	 ***************************************************************/
 	
-	// TODO: improve syntax of bbb.assert
-	// TODO: multiple asserts and multiple variable interconnected
-	// TODO: enable and disable AssertConstraints
-	// TODO: integration with other solvers
-	
-	Error.subclass("ContinuousAssertError", {
-		initialize: function ContinuousAssertError(msg) {
-			if (Error.captureStackTrace) {
-				Error.captureStackTrace(this, ContinuousAssertError);
-			} else {
-				this.stack = (new Error).stack || '';
-			}
-			this.message = msg;
-		},
-		name: "ContinuousAssertError"
-	});
-	
-	Object.subclass("AssertSolver", {
-		initialize: function(message) {
-			this.message = message;
-		},
+	Object.subclass("ReactiveSolver", {
 	    isConstraintObject: function() { return true; },
-	    always: function(opts, func) {
-	    	//console.log("AssertSolver.always");
-	        var ctx = opts.ctx;
-	        func.varMapping = ctx;
-	        var cobj = new Constraint(func, this);
-			cobj.allowFailing = true;
-	        cobj.addPrimitiveConstraint(new AssertSolver.Constraint(this, cobj, func));
-			try {
-				if(!opts.postponeEnabling) { cobj.enable(); }
-			} catch(e) {
-				if(e instanceof ContinuousAssertError) {
-					cobj.disable();
-				}
-				throw e;
-			}
-	        return cobj;
-	    },
 	    constraintVariableFor: function(value, ivarname, bbbCVar) {
-	    	//console.log("AssertSolver.constraintVariableFor", value, ivarname, bbbCVar);
-	    	return new AssertSolver.Variable(this, value, ivarname, bbbCVar);
-	    },
-	    solve: function() {
-	    	//console.log("AssertSolver.solve");
-	    	this.check();
-	    },
-	    check: function() {
-	    	//console.log("AssertSolver.check");
-	    	if(this.constraint && this.constraint.enabled && typeof this.constraint.predicate === "function")
-	    		if(!this.constraint.predicate())
-	    			throw new ContinuousAssertError(this.message);
+	    	return new ReactiveSolver.Variable(this, value, ivarname, bbbCVar);
 	    },
 	    weight: 10
 	});
 	
-	Object.subclass("AssertSolver.Variable", {
+	Object.subclass("ReactiveSolver.Variable", {
+	    isConstraintObject: function() { return true; },
 		initialize: function(solver, value, ivarname, bbbCVar) {
-	    	//console.log("AssertSolver.Variable.initialize");
 			this.solver = solver;
 			this.__val__ = value;
 		},
-	    isConstraintObject: function() { return true; },
-	    toFulfill: function(assertPredicate) {
-	    	//console.log("AssertSolver.Variable.toFulfill", assertPredicate);
-	    	this.assertPredicate = assertPredicate;
-	    },
 	    suggestValue: function(value) {
-	    	//console.log("AssertSolver.Variable.suggestValue", value);
 			var prev = this.__val__;
 	    	this.__val__ = value;
 			try {
@@ -100,63 +46,90 @@ module('users.timfelgentreff.experimental.assert').requires('users.timfelgentref
 	    	return this.__val__;
 	    },
 	    setReadonly: function(bool) {
-	    	//console.log("AssertSolver.Variable.setReadonly", bool);
 	    	this.readonly = bool;
 	    },
 	    isReadonly: function() {
-	    	//console.log("AssertSolver.Variable.isReadonly");
 	        return this.readonly;
 	    },
 	    cnEquals: function() {
-	    	//console.log("AssertSolver.Variable.cnEquals");
-	        return new AssertSolver.PrimitiveConstraint(this, arguments);
+	        return new ReactiveSolver.PrimitiveConstraint(this, arguments);
 	    }
 	});
 	
-	Object.subclass("AssertSolver.PrimitiveConstraint", {
+	Object.subclass("ReactiveSolver.PrimitiveConstraint", {
 	    isConstraintObject: function() { return true; },
 	    initialize: function(variable, args) {
-	    	//console.log("AssertSolver.PrimitiveConstraint.initialize", variable, args);
 			this.enabled = false;
 	    	this.solver = variable.solver;
 	    },
 	    enable: function() {
-	    	//console.log("AssertSolver.PrimitiveConstraint.enable");
 			this.enabled = true;
 	    },
 	    disable: function() {
-	    	//console.log("AssertSolver.PrimitiveConstraint.disable");
 			this.enabled = false;
 	    }
 	});
 	
-	Object.subclass("AssertSolver.Constraint", {
+	Object.subclass("ReactiveSolver.Constraint", {
 	    isConstraintObject: function() { return true; },
 	    initialize: function(solver, bbbConstraint, func) {
-	    	//console.log("AssertSolver.Constraint.initialize");
 			this.enabled = false;
 	    	this.solver = solver;
 	    	this.solver.constraint = this;
 			this.bbbConstraint = bbbConstraint;
 
-	    	// extract predicate from bbbConstraint
-	    	this.predicate = func;/*_.chain(bbbConstraint.constraintvariables)
-	    		.map(function(variable) {
-	    			return variable.externalVariables(this.solver);
-	    		}, this)
-	    		.find(function(externalVariable) {
-	    			return typeof externalVariable.assertPredicate === "function";
-	    		})
-	    		.value()
-	    		//.assertPredicate;*/
+	    	this.predicate = func;
 	    },
 	    enable: function() {
-	    	//console.log("AssertSolver.Constraint.enable");
 			this.enabled = true;
 	    },
 	    disable: function() {
-	    	//console.log("AssertSolver.Constraint.disable");
 			this.enabled = false;
+	    }
+	});
+	
+	/***************************************************************
+	 * Continuous asserts
+	 ***************************************************************/
+	
+	// TODO: integration with other solvers
+	
+	Error.subclass("ContinuousAssertError", {
+		initialize: function ContinuousAssertError(msg) {
+			if (Error.captureStackTrace) {
+				Error.captureStackTrace(this, ContinuousAssertError);
+			} else {
+				this.stack = (new Error).stack || '';
+			}
+			this.message = msg;
+		},
+		name: "ContinuousAssertError"
+	});
+	
+	ReactiveSolver.subclass("AssertSolver", {
+		initialize: function(message) {
+			this.message = message;
+		},
+	    always: function(opts, func) {
+	        var ctx = opts.ctx;
+	        func.varMapping = ctx;
+	        var cobj = new Constraint(func, this);
+			cobj.allowFailing = true;
+	        cobj.addPrimitiveConstraint(new ReactiveSolver.Constraint(this, cobj, func));
+			try {
+				if(!opts.postponeEnabling) { cobj.enable(); }
+			} catch(e) {
+				if(e instanceof ContinuousAssertError) {
+					cobj.disable();
+				}
+				throw e;
+			}
+	        return cobj;
+	    },
+	    solve: function() {
+	    	if(this.constraint && this.constraint.enabled && typeof this.constraint.predicate === "function")
+	    		if(!this.constraint.predicate())
+	    			throw new ContinuousAssertError(this.message);
 	    }
 	});
 	
@@ -179,17 +152,15 @@ module('users.timfelgentreff.experimental.assert').requires('users.timfelgentref
 			this.triggeredOnce = false;
 		},
 	    always: function(opts, func) {
-	    	//console.log("TriggerSolver.always");
 	        var ctx = opts.ctx;
 	        func.varMapping = ctx;
 	        var cobj = new Constraint(func, this);
 			cobj.allowFailing = true;
-	        cobj.addPrimitiveConstraint(new AssertSolver.Constraint(this, cobj, func));
+	        cobj.addPrimitiveConstraint(new ReactiveSolver.Constraint(this, cobj, func));
 			if(!opts.postponeEnabling) { cobj.enable(); }
 	        return cobj;
 	    },
 	    solve: function() {
-	    	//console.log("TriggerSolver.solve", this.constraint.predicate());
 	    	if(this.constraint &&
 				this.constraint.enabled &&
 				typeof this.constraint.predicate === "function"
@@ -225,17 +196,15 @@ module('users.timfelgentreff.experimental.assert').requires('users.timfelgentref
 			this.layer = layer;
 		},
 	    always: function(opts, func) {
-	    	//console.log("LayerActivationSolver.always");
 	        var ctx = opts.ctx;
 	        func.varMapping = ctx;
 	        var cobj = new Constraint(func, this);
 			cobj.allowFailing = true;
-	        cobj.addPrimitiveConstraint(new AssertSolver.Constraint(this, cobj, func));
+	        cobj.addPrimitiveConstraint(new ReactiveSolver.Constraint(this, cobj, func));
 			cobj.enable();
 	        return cobj;
 	    },
 	    solve: function() {
-	    	//console.log("LayerActivationSolver.solve", this.constraint.predicate());
 	    	if(this.constraint &&
 				this.constraint.enabled &&
 				typeof this.constraint.predicate === "function"
@@ -251,8 +220,7 @@ module('users.timfelgentreff.experimental.assert').requires('users.timfelgentref
 	    weight: 10
 	});
 	
-	// hotfix
-	// instead of cop.GlobalLayers.removeAt(idx); just use standard conform splice method
+	// HACK: instead of cop.GlobalLayers.removeAt(idx); just use standard conform splice method
 	Object.extend(cop, {
 		disableLayer: function(layer) {
 			var idx = cop.GlobalLayers.indexOf(layer)
@@ -350,5 +318,4 @@ module('users.timfelgentreff.experimental.assert').requires('users.timfelgentref
 			return copDisableLayer.apply(this, arguments);
 		}
 	});
-
 });
