da11e1d2e7b85633d36e7df1310c3f8740475f28
Tim Felgentreff <timfelgentreff@gmail.com>
2014-09-22 19:30:42 +0200

    updated playground with some more tests, added a Doubler part to the bin

--- borning/playground.js
+++ borning/playground.js
@@ -4,75 +4,44 @@ TestCase.subclass('users.borning.playground.PlaygroundTests', {
     testPointAdd: function() {
         var pt1 = pt(10, 15),
             pt2 = pt(20, 35),
-            pt3 = pt(0, 0);
-        (function () {
-            return pt1.addPt(pt2).equals(pt3);
-        }).shouldBeTrue({pt1: pt1, pt2: pt2, pt3: pt3});
-        this.assert(pt1.addPt(pt2).equals(pt3));
+            pt3 = pt(30, 50);
+            this.assert(pt1.addPt(pt2).equals(pt3));
     },
-    testSimple2: function() {
+    testPointAdditionConstraint: function() {
+        // test a constraint that adds two points
+        var p1 = pt(10,15), p2 = pt(20,35), p3=pt(0,0);
+        bbb.always({
+            // debugging: true
+            solver: new ClSimplexSolver(),
+            ctx: {
+                ClSimplexSolver: ClSimplexSolver,
+                p1: p1,
+                p2: p2,
+                p3: p3,
+                pt: pt,
+                _$_self: this.doitContext || this
+            }
+        }, function() {
+            return p1.addPt(p2).equals(p3) && p1.equals(pt(100, 300)) && p3.x == 145 && p3.y == 500;;
+        });         
+        this.assert(p1.equals(pt(100,300)));
+        this.assert(p2.equals(pt(45,200)));
+        this.assert(p3.equals(pt(145,500)));
+        
+        // original constraint follows:
+        //    always: {
+        //    // debugging: true
+        //    solver: new ClSimplexSolver()
+        //    p1.addPt(p2).equals(p3) && p1.equals(pt(100,300))  &&
+        //	p3.x==45 && p3.y==500;
+        //    } 
+    },
+    testEquals: function() {
+        // very simple assertion test
         var x = 5;
-        this.assert(x==5);
-    },
-    testSimple: function() {
-        var pt1 = pt(10, 15);
-        this.assert(pt1.equals(pt1));
-    },
-testPointAdd: function() {
-    var pt1 = pt(10, 15),
-    pt2 = pt(20, 35),
-    pt3 = pt(0, 0);
-    bbb.always({
-        ctx: {
-            pt1: pt1,
-            pt2: pt2,
-            pt3: pt3,
-            _$_self: this.doitContext || this
-        }
-    }, function() {
-        return pt1.addPt(pt2).equals(pt3);;
-    });
-        this.assert(pt1.addPt(pt2).equals(pt3));
+        this.assert(x==5)
     }
-    
-    //        this.assert(pt1.addPt(pt2).equals(pt3));
-
-    
-    //        this.assert(pt1.addPt(pt2).equals(pt3));
-
-    
-    //        this.assert(pt1.addPt(pt2).equals(pt3));
-
-    
-    
-    
-//            this.assert(expected.equals(pt3));
-
-    
-    
-    
-//            this.assert(expected.equals(pt3));
-
-    
-    
-    
-//            this.assert(expected.equals(pt3));
-
-    
-    
-    
-//            this.assert(expected.equals(pt3));
-,
-    testPointEquals: function() {
-        var pt1 = pt(10, 10),
-            pt2 = pt(20, 20);
-        (function () {
-            return pt1.equals(pt2);
-        }).shouldBeTrue({pt1: pt1, pt2: pt2});
-        this.assert(pt1.equals(pt2));
-    },
-
-
+   
 })
 
 }); // end of module
