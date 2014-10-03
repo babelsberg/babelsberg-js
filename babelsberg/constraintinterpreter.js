30f02245dbd99cbc93217817a7e5401a650b61f9
Tim Felgentreff <timfelgentreff@gmail.com>
2014-10-03 20:34:56 +0200

    fix typo

--- babelsberg/constraintinterpreter.js
+++ babelsberg/constraintinterpreter.js
@@ -796,7 +796,7 @@ Object.subclass('ConstrainedVariable', {
             return {recv: this.recv, getter: this.getter, setter: this.setter};
         } else {
             if (this.parentConstrainedVariable) {
-                this.setter = return this.parentConstrainedVariable.findOptionalSetter();
+                return this.setter = this.parentConstrainedVariable.findOptionalSetter();
             }
         }
     },
