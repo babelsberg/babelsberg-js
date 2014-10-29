4ac010d6b727ce586b555c01f5148dd6bd091b14
Stefan Lehmann <stefan.lehmann@student.hpi.uni-potsdam.de>
2014-10-29 18:04:25 +0100

    introduced game class

--- standalone/examples/robot/lib/we_play_tanks.js
+++ standalone/examples/robot/lib/we_play_tanks.js
@@ -1,166 +1,199 @@
-window.onload = function() {
-	var canvasId = "game";
-	
-	// prepare stats
-	var stats = new Stats();
-	stats.domElement.style.position = 'absolute';
-	stats.domElement.style.left = '0px';
-	stats.domElement.style.top = '0px';
-	document.body.appendChild( stats.domElement );
+Object.subclass("Game", {
+    initialize: function(canvasId) {
+        this.buildCanvas(canvasId);
+        this.buildInput(canvasId);
+        this.renderer = new Renderer(this.canvas);
+        this.buildViewport();
+        this.constrainDebugLayer();
+        this.init();
+    },
+    init: function() {},
+    buildCanvas: function(canvasId) {
+        this.canvas = document.getElementById(canvasId);
+        this.canvas.style.position = "absolute";
+        this.canvas.style.top = "0px";
+        this.canvas.style.left = "0px";
+        this.canvas.style["z-index"] = -1;
+    },
+    buildInput: function(canvasId) {
+        this.input = new Input(canvasId);
+        this.input.initKeyboard();
+        this.input.initMouse();
 
-	// setup game
-	var canvas = document.getElementById(canvasId);
-	canvas.style.position = "absolute";
-	canvas.style.top = "0px";
-	canvas.style.left = "0px";
-	canvas.style["z-index"] = -1;
-	
-	var renderer = new Renderer(canvas);
-	
-	var input = new Input(canvasId);
-	input.initKeyboard();
-	input.initMouse();
+        this.input.bind(Input.KEY.W, "up");
+        this.input.bind(Input.KEY.A, "left");
+        this.input.bind(Input.KEY.S, "down");
+        this.input.bind(Input.KEY.D, "right");
 
-	input.bind(Input.KEY.W, "up");
-	input.bind(Input.KEY.A, "left");
-	input.bind(Input.KEY.S, "down");
-	input.bind(Input.KEY.D, "right");
+        this.input.bind(Input.KEY.UP_ARROW, "up");
+        this.input.bind(Input.KEY.LEFT_ARROW, "left");
+        this.input.bind(Input.KEY.DOWN_ARROW, "down");
+        this.input.bind(Input.KEY.RIGHT_ARROW, "right");
 
-	input.bind(Input.KEY.UP_ARROW, "up");
-	input.bind(Input.KEY.LEFT_ARROW, "left");
-	input.bind(Input.KEY.DOWN_ARROW, "down");
-	input.bind(Input.KEY.RIGHT_ARROW, "right");
+        this.input.bind(Input.KEY.MOUSE1, "leftclick");
+        this.input.bind(Input.KEY.MOUSE2, "rightclick");
+        this.input.bind(Input.KEY.MWHEEL_UP, "zoomIn");
+        this.input.bind(Input.KEY.MWHEEL_DOWN, "zoomOut");
 
-	input.bind(Input.KEY.MOUSE1, "leftclick");
-	input.bind(Input.KEY.MOUSE2, "rightclick");
-	input.bind(Input.KEY.MWHEEL_UP, "zoomIn");
-	input.bind(Input.KEY.MWHEEL_DOWN, "zoomOut");
+        this.input.bind(Input.KEY.P, "debug");
+    },
+    buildViewport: function() {
+        var input = this.input,
+            viewport = this.viewport = new Viewport(
+                new Vector2(30, 30* this.canvas.height/this.canvas.width),
+                new Vector2(60, 60 * this.canvas.height/this.canvas.width)
+            );
 
-	input.bind(Input.KEY.P, "debug");
-
-	var viewport = new Viewport(
-		new Vector2(30, 30* canvas.height/canvas.width),
-		new Vector2(60, 60 * canvas.height/canvas.width)
-	);
-
-    // constraint:
-    // - keep input position in world and mouse on screen in sync
-    bbb.always({
-        solver: new DBPlanner(),
-        ctx: {
-            input: input,
-            viewport: viewport
-        },
-        methods: function() {
-            input.position.formula([input.mouse, input.mouse.x, input.mouse.y], function(mouse, mouseX, mouseY) {
-                return viewport.screenToWorldCoordinates(mouse);
-            });
-        }
-    }, function() {
-        return input.position.equals(viewport.screenToWorldCoordinates(input.mouse));
-    });
-
-    // do not debugdraw velocities if debug button is pressed
-    cop.create("debugLayer")
-        .activeOn({
+        // constraint:
+        // - keep input position in world and mouse on screen in sync
+        bbb.always({
+            solver: new DBPlanner(),
             ctx: {
-                input: input
+                input: input,
+                viewport: viewport
+            },
+            methods: function() {
+                input.position.formula([input.mouse, input.mouse.x, input.mouse.y], function(mouse, mouseX, mouseY) {
+                    return viewport.screenToWorldCoordinates(mouse);
+                });
             }
         }, function() {
-            return input.switchedOn("debug") == true;
-        })
-        .refineClass(GameObject, {
-            draw: function(renderer) {
-                cop.proceed(renderer);
-           		renderer.drawLine(this.position, this.position.add(this.velocity), "red", 1, 3);
-            }
-        })
-        .refineClass(CPUControls, {
-            getTargetTiles: function() {
-                var tiles = cop.proceed();
-
-                tiles.each(function(tile) {
-                    tile.marked = tile.canFlyThrough() ? this.color : "red";
-                }, this);
-
-                return tiles;
-            }
+            return input.position.equals(viewport.screenToWorldCoordinates(input.mouse));
         });
+    },
+    constrainDebugLayer: function() {
+        var input = this.input;
 
-	var world, gui;
+        // do not debugdraw velocities if debug button is pressed
+        cop.create("debugLayer")
+            .activeOn({
+                ctx: {
+                    input: input
+                }
+            }, function() {
+                return input.switchedOn("debug") == true;
+            })
+            .refineClass(GameObject, {
+                draw: function(renderer) {
+                    cop.proceed(renderer);
+                    renderer.drawLine(this.position, this.position.add(this.velocity), "red", 1, 3);
+                }
+            })
+            .refineClass(CPUControls, {
+                getTargetTiles: function() {
+                    var tiles = cop.proceed();
 
-	var init = function() {
-        world = new World(new AABB(
+                    tiles.each(function(tile) {
+                        tile.marked = tile.canFlyThrough() ? this.color : "red";
+                    }, this);
+
+                    return tiles;
+                }
+            });
+    },
+    prepare: function() {
+        this.world = new World(new AABB(
             new Vector2(-150, -150),
             new Vector2(150, 150)
-        ), input, viewport);
+        ), this.input, this.viewport);
 
-        gui = new Gui(world, input, player, viewport);
-	};
+        this.gui = new Gui(this.world, this.input, player, this.viewport);
+    },
+    update: function(dt) {
+        this.updatePhysics(dt);
+        this.gui.update(dt);
+        this.draw();
 
-	// frame update
-	var downPoint = Vector2.Zero.copy();
-	var lastPoint = Vector2.Zero.copy();
-	var update = function(dt) {
-		if(input.pressed("leftclick")) {
-			downPoint.set(input.mouse);
-		}
-		if(input.pressed("rightclick")) {
-			lastPoint.set(input.mouse);
-		}
-		// viewport manipulation
-		if(input.state("rightclick")) {
-			viewport.translateBy(canvas, lastPoint.sub(input.mouse));
-			lastPoint.set(input.mouse);
-		}
-		if(input.state("zoomIn")) {
-			viewport.zoomIn();
-		}
-		if(input.state("zoomOut")) {
-			viewport.zoomOut();
-		}
+        this.input.clearPressed();
+    },
+    updatePhysics: function(dt) {
+        this.world.update(dt);
+    },
+    draw: function() {
+        this.renderer.clear();
+        this.renderer.withViewport(this.viewport, (function() {
+            this.world.draw(this.renderer);
+        }).bind(this));
+        this.gui.draw(this.renderer);
+    }
+});
 
-		// update
-		world.update(dt);
-		gui.update(dt);
+cop.create("adjustViewportManuallyLayer")
+    .refineClass(Game, {
+        initialize: function(canvasId) {
+            cop.proceed(canvasId);
 
-		// drawing
-		renderer.clear();
-		renderer.withViewport(viewport, function() {
-			world.draw(renderer);
-		});
-		gui.draw(renderer);
+            this.downPoint = Vector2.Zero.copy();
+            this.lastPoint = Vector2.Zero.copy();
+        },
+        update: function(dt) {
+            if(this.input.pressed("leftclick")) {
+                this.downPoint.set(this.input.mouse);
+            }
+            if(this.input.pressed("rightclick")) {
+                this.lastPoint.set(this.input.mouse);
+            }
+            // viewport manipulation
+            if(this.input.state("rightclick")) {
+                this.viewport.translateBy(this.canvas, this.lastPoint.sub(this.input.mouse));
+                this.lastPoint.set(this.input.mouse);
+            }
+            if(this.input.state("zoomIn")) {
+                this.viewport.zoomIn();
+            }
+            if(this.input.state("zoomOut")) {
+                this.viewport.zoomOut();
+            }
 
-		input.clearPressed();
-	}
+            cop.proceed(dt);
+        }
+    })
+    .beGlobal();
 
-	// main loop
-	var lastFrame = window.performance.now();
-	function animate() {
-		stats.update();
+window.onload = function() {
+    var canvasId = "game",
+        game = new Game(canvasId);
 
-		// setup time since last call
-		var time = window.performance.now();
-		var dt = (time - lastFrame) / 1000;
-		lastFrame = time;
+    // prepare stats
+    var stats = new Stats();
+    stats.domElement.style.position = 'absolute';
+    stats.domElement.style.left = '0px';
+    stats.domElement.style.top = '0px';
+    document.body.appendChild( stats.domElement );
 
-		update(dt);
-		requestAnimationFrame(animate);
-	}
-	
-	// asset loading
-	queue()
-		.defer(loadImage, "assets/tileset.png")
-		.defer(loadImage, "assets/tank.png")
-		.defer(loadImage, "assets/turret.png")
-		.defer(loadImage, "assets/bullet.png")
-		.defer(loadImage, "assets/target.png")
-		.await(function(error) {
-			if(error) {
-				console.error("error while loading");
-			} else {
-			    init();
-				animate();
-			}
-		});
-}
+    // frame update
+    var update = function(dt) {
+        game.update(dt);
+    }
+
+    // main loop
+    var lastFrame = window.performance.now();
+    function animate() {
+        stats.update();
+
+        // setup time since last call
+        var time = window.performance.now();
+        var dt = (time - lastFrame) / 1000;
+        lastFrame = time;
+
+        update(dt);
+        requestAnimationFrame(animate);
+    }
+
+    // asset loading
+    queue()
+        .defer(loadImage, "assets/tileset.png")
+        .defer(loadImage, "assets/tank.png")
+        .defer(loadImage, "assets/turret.png")
+        .defer(loadImage, "assets/bullet.png")
+        .defer(loadImage, "assets/target.png")
+        .await(function(error) {
+            if(error) {
+                console.error("error while loading");
+            } else {
+                game.prepare();
+
+                animate();
+            }
+        });
+};
