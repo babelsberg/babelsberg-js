f6f36e7580b3d944fa30a2c09e274bc5161d67a0
Stefan Lehmann <stefan.lehmann@student.hpi.uni-potsdam.de>
2014-11-03 13:37:07 +0100

    removed unused code

--- standalone/examples/robot/lib/rendering/image.js
+++ standalone/examples/robot/lib/rendering/image.js
@@ -1,32 +1,3 @@
-/*
-rts.Image = Resource.subclass({
-	initialize: function(path) {
-		Resource.prototype.initialize.apply(this, arguments);
-		this.path = path;
-	},
-	
-	load: function(callback) {
-		this.data = new window.Image();
-		this.data.onload = callback;
-		this.data.onerror = callback;
-		this.data.src = "data/graphics/" + this.path;
-	},
-	
-	draw: function(aabb, sourceX, sourceY, width, height) {
-		env.renderer.drawImageOnWorldAABB(this.data, aabb, sourceX, sourceY, width, height);
-	}
-});
-
-rts.Image.cache = {};
-rts.Image.get = function(path) {
-	if(typeof rts.Image.cache[path] === "undefined") {
-		rts.Image.cache[path] = new rts.Image(path);
-	};
-	
-	return rts.Image.cache[path];
-};
-*/
-
 var loadImage = function(path, callback) {
 	var img = new window.Image();
 	img.onload = function loaded() { callback(null, img)};
