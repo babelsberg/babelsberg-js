cef46195e5e77deca9e27a3a3f06729efdbaf606
Tim Felgentreff <timfelgentreff@gmail.com>
2015-01-23 10:36:20 +0100

    initially make colors an invalid value

--- examples/csp/map.js
+++ examples/csp/map.js
@@ -77,7 +77,7 @@ contentLoaded(window, function() {
                 return {
                     geometry: geometry,
                     name: state.properties.name,
-                    color: colors[0]
+                    color: 0
                 };
             })
             .map(function(state) {
