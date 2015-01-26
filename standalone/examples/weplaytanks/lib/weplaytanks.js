d117c7c238dbd7c27d15619a720cd4cc38ee9079
Stefan Lehmann <stefan.lehmann@student.hpi.uni-potsdam.de>
2015-01-26 11:36:00 +0100

    updated level selection

--- standalone/examples/weplaytanks/lib/weplaytanks.js
+++ standalone/examples/weplaytanks/lib/weplaytanks.js
@@ -14,62 +14,52 @@ require([
     Game,
     PluginLoader
 ) {
-    //window.onload = function() {
-        var canvasId = "game",
-            game = new Game(canvasId);
+    var canvasId = "game",
+        game = new Game(canvasId);
 
-        // prepare stats
-        var stats = new Stats();
-        stats.domElement.style.position = 'absolute';
-        stats.domElement.style.left = '0px';
-        stats.domElement.style.top = '0px';
-        document.body.appendChild( stats.domElement );
+    // prepare stats
+    var stats = new Stats();
+    stats.domElement.style.position = 'absolute';
+    stats.domElement.style.left = '0px';
+    stats.domElement.style.top = '0px';
+    document.body.appendChild( stats.domElement );
 
-        // main loop
-        var time = new Time();
-        function animate() {
-            stats.update();
-            var dt = time.update();
-            game.update(dt);
-        }
-        var loop = new Loop(animate);
+    // main loop
+    var time = new Time();
+    function animate() {
+        stats.update();
+        var dt = time.update();
+        game.update(dt);
+    }
+    var loop = new Loop(animate);
 
-        // asset loading
-        queue()
-            /*
-             * Demo Maps
-             */
-            .defer(loadLevel, 0, 'game/0_tutorial.json')
-            .defer(loadLevel, 1, 'game/1_movingtank.json')
-            .defer(loadLevel, 2, 'demo/2_twotanks.json')
-            .defer(loadLevel, 3, 'demo/3_singlehunter.json')
-            .defer(loadLevel, 4, 'game/5_borderline.json')
-            .defer(loadLevel, 5, 'game/6_hunter2.json')
-            .defer(loadLevel, 6, 'demo/7_demo.json')
-            /*
-             * Game Maps
-            .defer(loadLevel, 0, 'game/0_tutorial.json')
-            .defer(loadLevel, 1, 'game/1_movingtank.json')
-            .defer(loadLevel, 2, 'game/2_multipletanks.json')
-            .defer(loadLevel, 3, 'game/3_grid.json')
-            .defer(loadLevel, 4, 'game/4_hunter.json')
-            .defer(loadLevel, 5, 'game/5_borderline.json')
-            .defer(loadLevel, 6, 'game/6_hunter2.json')
-             */
-            .defer(loadImage, "tileset.png")
-            .defer(loadImage, "tank.png")
-            .defer(loadImage, "turret.png")
-            .defer(loadImage, "bullet.png")
-            .defer(loadImage, "target.png")
-            .defer(loadImage, "powerups.png")
-            .await(function(error) {
-                if(error) {
-                    console.error("error while loading", error);
-                } else {
-                    console.log(arguments[1]);
-                    game.prepare();
-                    loop.start();
-                }
-            });
-    //};
+    // asset loading
+    queue()
+        .defer(loadLevel, 0, 'game/0_tutorial.json')
+        .defer(loadLevel, 1, 'game/1_movingtank.json')
+        .defer(loadLevel, 2, 'demo/2_twotanks.json')
+        .defer(loadLevel, 3, 'demo/3_singlehunter.json')
+        .defer(loadLevel, 4, 'game/5_borderline.json')
+        .defer(loadLevel, 5, 'game/6_hunter2.json')
+        .defer(loadLevel, 6, 'demo/7_demo.json')
+        .defer(loadLevel, 7, 'game/2_multipletanks.json')
+        .defer(loadLevel, 8, 'game/3_grid.json')
+        .defer(loadLevel, 9, 'game/4_hunter.json')
+
+        .defer(loadImage, "tileset.png")
+        .defer(loadImage, "tank.png")
+        .defer(loadImage, "turret.png")
+        .defer(loadImage, "bullet.png")
+        .defer(loadImage, "target.png")
+        .defer(loadImage, "powerups.png")
+
+        .await(function(error) {
+            if(error) {
+                console.error("error while loading", error);
+            } else {
+                console.log(arguments[1]);
+                game.prepare();
+                loop.start();
+            }
+        });
 });
