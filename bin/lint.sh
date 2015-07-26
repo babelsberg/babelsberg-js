f4ba9457d125fbcf64803feaa2055f0345b009f5
Fabio Niephaus <code@fniephaus.com>
2015-07-26 16:55:35 +0200

    Add emscripten port of rhea (https://github.com/Nocte-/rhea)

--- bin/lint.sh
+++ bin/lint.sh
@@ -9,7 +9,7 @@ if [ $? -ne 0 ]; then
 fi
 
 FOLDERS=(babelsberg cassowary csp deltablue standalone sutherland)
-EXCLUDE_FILES="uglify.js,PerformanceTests.js,tests.js,src_transform_test.js,zombietest.js,prototype.js,test_harness.js,underscore-min.js,testsuite.js,cassowaryjs.js"
+EXCLUDE_FILES="uglify.js,PerformanceTests.js,tests.js,src_transform_test.js,zombietest.js,prototype.js,test_harness.js,underscore-min.js,testsuite.js,cassowaryjs.js,rhea.js,rhea.emscripten.js"
 EXCLUDE_FOLDERS="examples"
 CUSTOM_JSDOC_TAGS="example,function,global,name,tutorial"
 MAX_LINE_LEN=90
